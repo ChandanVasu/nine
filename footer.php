f46675320b475a9bac31561a0e470db4b13eb136
Chandan Vasu <chandanyadavparas@gmail.com>
2024-05-07 13:19:10 +0530

    fix

--- footer.php
+++ footer.php
@@ -1,14 +1,16 @@
 <?php
 global $nine_theme;
+
+// Check if footer_text is defined and not empty
+$footer_text = !empty($nine_theme['footer_text']) ? $nine_theme['footer_text'] : 'VasuTheme All Rights Reserved';
 ?>
 
 <!-- Display in the footer -->
 <footer>
-    <div class="footer-content" style="background-color: <?php echo esc_attr($nine_theme['body_bg_color']); ?>;">
-        <p><?php echo esc_html($nine_theme['footer_text']); ?></p>
-    </div>
+<div class="footer-content" >
+    <p><?php echo esc_html($footer_text); ?></p>
+
+</div>
 </footer>
 
 <?php wp_footer(); ?>
-
-<!-- theme_get_option('footer_text'); -->
