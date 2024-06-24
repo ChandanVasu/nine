3ab1640760e18ce97af1ac07ab4032a3df653f04
Chandan Vasu <chandanyadavparas@gmail.com>
2024-06-24 16:04:26 +0530

    fix bug single.php

--- single.php
+++ single.php
@@ -6,19 +6,19 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package nine
+ * @package Nine
  */
 
 get_header();
 
 // Get the template ID and selected header from options
-$nine_template_id = nine_get_opt('single_template');
+$template_id = nine_get_opt('single_template');
 $nine_template = nine_get_opt('single_select', 'single-one');
 
 // Function to get the template part based on the selected header
-if (!function_exists('nine_get_selected_single')) {
-    function nine_get_selected_single($template) {
-        $template_path = 'template/single-post/' . $template;
+if (!function_exists('get_selected_single')) {
+    function get_selected_single($nine_template) {
+        $template_path = 'template/single-post/' . sanitize_file_name($nine_template);
         if (locate_template($template_path . '.php')) {
             get_template_part($template_path);
         } else {
@@ -30,18 +30,26 @@ if (!function_exists('nine_get_selected_single')) {
 
 // Check if there are posts
 if (have_posts()) :
-    while (have_posts()) : the_post();
-        if ($nine_template_id && function_exists('nine_display_core_content')) {
-            $elementor_content = nine_display_core_content($nine_template_id);
-            if ($elementor_content) {
-                echo apply_filters('nine_header', $elementor_content);
-                continue; // Move to the next post
+    ?>
+    <div class="nine-single-post">
+        <?php
+        while (have_posts()) : the_post();
+            if ($template_id && function_exists('display_nine_core_content')) {
+                $elementor_content = display_nine_core_content($template_id);
+                if ($elementor_content) {
+                    echo apply_filters('nine_single', $elementor_content); // Assume content is properly escaped in filter
+                    continue; // Move to the next post
+                }
             }
-        }
-        nine_get_selected_single($nine_template);
-    endwhile;
+            get_selected_single($nine_template);
+        endwhile;
+        ?>
+    </div>
+    <?php
 else :
-    echo '<p>No content found</p>';
+    ?>
+    <p><?php esc_html_e('No content found', 'nine'); ?></p>
+    <?php
 endif;
 
 get_footer();
